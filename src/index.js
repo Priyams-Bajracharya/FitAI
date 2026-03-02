a369af4d7570e21976223b9fcb98ac1f5c16c13b
priyams <priyams.bajracharya@gmail.com>
2026-03-02 23:12:12 +0545

    initial commit - project setup and structure

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()//loads .env file variables into process.env
+
+const app = express()
+
+app.use(express.json())//tells express to automatically parse incoming json to use req.body
+
+app.get('/health', (req,res)=>{
+  res.status(200).json({ message: "FitAI server is running"})
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT , ()=>{
+  console.log(`Server is running on port ${PORT}`)
+})
