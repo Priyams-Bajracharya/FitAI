e1ee4bcee11084d2eb6bd521f0753a1492464321
priyams <priyams.bajracharya@gmail.com>
2026-03-03 09:34:26 +0545

    added comments to db connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,15 @@
+
 const {Pool} =require('pg')
+//destructuring , we take the pool property from the obj pg , 
+
+//a pool is like having multiple waiters ready to server , so a pool helps to handle multiple requests simultaneously , in real app multiple users might hit the  api at the same time , a single connection would make them queue one by one , a pool handles them concurrently 
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 })
 
+
+//.then and .catch are promises in js , pool.connect takes time and js doesnt wait , it moves on and comes back when the jobs done , a promise represents "I'll give the results later", .then runs when operation succeeded , .catch runs when the operation failed 
 pool.connect()
 .then(()=>{
   console.log('Connected to PostgreSQL')
